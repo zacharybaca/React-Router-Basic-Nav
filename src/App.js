044f433597d75632de824c1c364632cbbd8adf53
zacharybaca <baca.zach@icloud.com>
2019-09-16 14:46:44 -0500

    MVP Completed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => (
   <div>
     <Navigation />
 
-    //Routes
+  
   <Route exact path="/" component={Home} />
   <Route path="/about" component={About} />
   <Route path="/contact" component={Contact} />
